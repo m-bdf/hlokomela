df06786e30dac8bfec1ca77fa3f1a47be5a09f15
Maëlys Bras de fer <bastien.bras-de-fer@epitech.eu>
2021-11-10 22:09:53 +0100

    [+] wrap responses in json objects

--- api/main.py
+++ api/main.py
@@ -18,60 +18,60 @@ notes = {
 @app.route('/login', methods=['POST'])
 def login_route():
   if not request.is_json:
-    return "no json data", 400
+    return {'error': "no json data"}, 400
   try:
     username = request.json['username']
     password = request.json['password']
   except KeyError as e:
-    return f"missing {e}", 400
+    return {'error': f"missing {e}"}, 400
   if users.get(username) != password:
-    return "bad credentials", 401
-  return SECRET
+    return {'error': "bad credentials"}, 401
+  return {'token': SECRET}
 
 
 @app.route('/register', methods=['POST'])
 def register_route():
   if not request.is_json:
-    return "no json data", 400
+    return {'error': "no json data"}, 400
   try:
     username = request.json['username']
     password = request.json['password']
   except KeyError as e:
-    return f"missing {e}", 400
+    return {'error': f"missing {e}"}, 400
   if username in users:
-    return "user already exists", 401
+    return {'error': "user already exists"}, 401
   users[username] = password
-  return SECRET
+  return {'token': SECRET}
 
 
 @app.route('/note', methods=['GET', 'PUT', 'DELETE'])
 def note_route():
   auth = request.headers.get('Authentication')
   if not auth or auth.split()[1] != SECRET:
-    return "invalid bearer token", 401
+    return {'error': "invalid bearer token"}, 401
 
   if request.method == 'GET':
-    return notes
+    return {'notes': notes}
 
   if request.method == 'PUT':
     if not request.is_json:
-      return "no json data", 400
+      return {'error': "no json data"}, 400
     try:
       title = request.json['title']
       content = request.json['content']
     except KeyError as e:
-      return f"missing {e}", 400
+      return {'error': f"missing {e}"}, 400
     notes[title] = content
 
   if request.method == 'DELETE':
     if not request.is_json:
-      return "no json data", 400
+      return {'error': "no json data"}, 400
     try:
       title = request.json['title']
     except KeyError as e:
-      return f"missing {e}", 400
+      return {'error': f"missing {e}"}, 400
     if title not in notes:
-      return "no such note", 404
+      return {'error': "no such note"}, 404
     del notes[title]
 
 
