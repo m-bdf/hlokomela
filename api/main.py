4c53da178158a0d71aa212479a6be7c9b8ce3a2b
Maëlys Bras de fer <bastien.bras-de-fer@epitech.eu>
2021-11-11 19:03:34 +0100

    [~] avoid code duplication

--- api/main.py
+++ api/main.py
@@ -1,10 +1,15 @@
 from flask import Flask, request
+from werkzeug import exceptions
 import os
 from collections import namedtuple
 import jwt
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+@app.errorhandler(exceptions.HTTPException)
+def wrap_error(e: exceptions.HTTPException):
+  return {'error': e.description}, e.code
 
 SECRET = os.urandom(16).hex()
 
@@ -13,85 +18,62 @@ User = namedtuple('user', 'password notes', defaults=[NOTES])
 users: dict[str, User] = {}
 
 
+def get_fields(*fields):
+  if not request.is_json:
+    raise exceptions.BadRequest("no json data")
+  try:
+    return [request.json[field] for field in fields]
+  except KeyError as e:
+    raise exceptions.BadRequest(f"missing {e}")
+
+
 @app.route('/login', methods=['POST'])
 def login_route():
-  if not request.is_json:
-    return {'error': "no json data"}, 400
-  try:
-    username = request.json['username']
-    password = request.json['password']
-  except KeyError as e:
-    return {'error': f"missing {e}"}, 400
+  username, password = get_fields('username', 'password')
   if username not in users or users[username].password != password:
-    return {'error': "bad credentials"}, 401
+    raise exceptions.Unauthorized("bad credentials")
   return {'token': jwt.encode({'iss': username}, SECRET)}
 
 
 @app.route('/register', methods=['POST'])
 def register_route():
-  if not request.is_json:
-    return {'error': "no json data"}, 400
-  try:
-    username = request.json['username']
-    password = request.json['password']
-  except KeyError as e:
-    return {'error': f"missing {e}"}, 400
+  username, password = get_fields('username', 'password')
   if username in users:
-    return {'error': "user already exists"}, 401
+    raise exceptions.Unauthorized("user already exists")
   users[username] = User(password)
   return {'token': jwt.encode({'iss': username}, SECRET)}
 
 
-@app.route('/notes')
-def notes_route():
+def get_notes():
   try:
     token = request.headers['Authentication'].split()[1]
     username = jwt.decode(token, SECRET)['iss']
-    notes = users[username].notes
+    return users[username].notes
   except:
-    return {'error': "invalid bearer token"}, 401
-  return {'titles': notes.keys()}
+    raise exceptions.Unauthorized("invalid bearer token")
+
+
+@app.route('/notes')
+def notes_route():
+  return {'titles': get_notes().keys()}
 
 
 @app.route('/note', methods=['GET', 'PUT', 'DELETE'])
 def note_route():
-  try:
-    token = request.headers['Authentication'].split()[1]
-    username = jwt.decode(token, SECRET)['iss']
-    notes = users[username].notes
-  except:
-    return {'error': "invalid bearer token"}, 401
+  notes = get_notes()
+  title = get_fields('title')
 
   if request.method == 'GET':
-    if not request.is_json:
-      return {'error': "no json data"}, 400
-    try:
-      title = request.json['title']
-    except KeyError as e:
-      return {'error': f"missing {e}"}, 400
     if title not in notes:
-      return {'error': "no such note"}, 404
+      raise exceptions.NotFound("no such note")
     return {'content': notes[title]}
 
   if request.method == 'PUT':
-    if not request.is_json:
-      return {'error': "no json data"}, 400
-    try:
-      title = request.json['title']
-      content = request.json['content']
-    except KeyError as e:
-      return {'error': f"missing {e}"}, 400
-    notes[title] = content
+    notes[title] = get_fields('content')
 
   if request.method == 'DELETE':
-    if not request.is_json:
-      return {'error': "no json data"}, 400
-    try:
-      title = request.json['title']
-    except KeyError as e:
-      return {'error': f"missing {e}"}, 400
     if title not in notes:
-      return {'error': "no such note"}, 404
+      raise exceptions.NotFound("no such note")
     del notes[title]
 
 
