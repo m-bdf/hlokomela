973500f63c24f890f8d893b5f40785a254001e44
mano.baffie <mano.baffie@epitech.eu>
2021-11-13 17:04:42 +0100

    add gestion listenner, for modify ip of api

--- hlokomela/lib/src/welcome/welcome_view.dart
+++ hlokomela/lib/src/welcome/welcome_view.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:hlokomela/src/login/login_view.dart';
+import 'package:hlokomela/src/service/call_api.dart';
 import 'package:hlokomela/src/widget/custom_button.dart';
+import 'package:shake/shake.dart';
 
-class WelcomeView extends StatelessWidget {
+class WelcomeView extends StatefulWidget {
   const WelcomeView({Key? key}) : super(key: key);
   static const String routeName = "/";
+
+  @override
+  State<WelcomeView> createState() => _WelcomeView();
+}
+
+class _WelcomeView extends State<WelcomeView> {
+
+  @override
+  void initState() {
+    ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
+      setState(() {
+        TextEditingController controller = TextEditingController(text: CallApi.route);
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Change ip server'),
+            content: const Text('Enter or modify IP'),
+            actions: <Widget>[
+              TextField(
+                controller: controller,
+              ),
+              TextButton(
+                onPressed: () =>
+                {Navigator.of(context).pop(controller.text)},
+                child: const Text('Validate'),
+              ),
+            ],
+          ),
+        ).then((value) => {
+          CallApi.route = controller.text
+        });
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
